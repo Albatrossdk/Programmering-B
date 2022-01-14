48610de3b7a10607fd025afa74c300090c0f2c99
Albatrossdk <leiferbjorn@gmail.com>
2022-01-14 18:37:05 +0100

    Flappybird finished

--- 113-P5-BOLD/index.js
+++ 113-P5-BOLD/index.js
@@ -7,10 +7,13 @@ let rectX, rectY, rectW, rectH
 let rectSpeed = 12
 let score = 0
 let randomizer = 50
+let lives = 10
+let once = true
+let fr = 60
 
 function setup(){
     createCanvas(windowWidth, windowHeight)
-    frameRate(120)
+    frameRate(fr)
     background('cornflowerblue')
     diameter = 32
     x = windowWidth/2
@@ -19,6 +22,7 @@ function setup(){
     rectH = 200
     rectX = windowWidth - rectW
     rectY = windowHeight - rectH
+    lives = 10
 }
 
 function showRect(){
@@ -31,8 +35,9 @@ function updateRect(){
     rectX -= rectSpeed
     if(rectX <= 0 - rectW){
         rectX = windowWidth
-        rectH = random(200, 360)
+        rectH = random(200, 300)
         rectY = windowHeight - rectH
+        once = true
     }
     if(rectX == windowWidth/2){
         score++
@@ -52,11 +57,11 @@ function update(){
     if(y > windowHeight - diameter/2){
         y = windowHeight - diameter/2
         velocity = -velocity
-        if(!alert('You lost')){window.location.reload();}
+        if(!alert('You touched the ground, YOU LOST!')){window.location.reload();}
     }
     if(y < 0){
         y = windowHeight/2
-        if(!alert('You lost')){window.location.reload();}
+        if(!alert('You flew too close to the sun, YOU LOST!')){window.location.reload();}
         x = 0
     }
     
@@ -68,10 +73,10 @@ function draw(){
     update()   
     showRect()
     updateRect()
-    select('#info').html(round(score))
+    select('#info').html(round(lives))
     collision()
-
-    
+    //console.log(frameCount)
+    frameincreaser()    
 }
 
 function keyPressed(key){
@@ -83,14 +88,32 @@ function keyPressed(key){
 
 function collision(){
     if(((x > rectX) && (x < rectX + rectW) &&
-    (y > rectY) && (y < rectY + rectH))){
-        x = 0
-        if(!alert('You lost')){window.location.reload();}        
+    (y > rectY) && (y < rectY + rectH) && (once))){
+        //if(!alert('You lost')){window.location.reload();}     
+        lives--
+        once = false    
     }
     if(((x > rectX) && (x < rectX + rectW) &&
-    (y > 0) && (y < 0 + rectH))){
+    (y > 0) && (y < 0 + rectH) && (once))){
+        //if(!alert('You lost')){window.location.reload();}
+        lives--
+        once = false
+    }
+    //else(once = true)
+
+    //lifechecker
+    if(lives == 0){
+        if(!alert('Out of lives, YOU LOST!')){window.location.reload();}
         x = 0
-        if(!alert('You lost')){window.location.reload();}        
+        lives = 10
     }
 
+}
+
+function frameincreaser(){
+    /*if(frameCount % 1000 == 0){
+        frameRate(fr++)
+        console.log('material gourl')
+        console.log(fr)
+    }*/
 }
