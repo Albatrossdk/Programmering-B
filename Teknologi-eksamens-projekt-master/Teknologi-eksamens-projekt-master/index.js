f11bb1302ea2198dcf33a856f50f11a1825ca3b4
Albatrossdk <leiferbjorn@gmail.com>
2022-04-26 15:44:10 +0200

    "finished cards on page2" And page 3 almost done.
    Co-authored-by: mierjar <mierjar@users.noreply.github.com>

--- Teknologi-eksamens-projekt-master/Teknologi-eksamens-projekt-master/index.js
+++ Teknologi-eksamens-projekt-master/Teknologi-eksamens-projekt-master/index.js
@@ -1,6 +1,7 @@
 let data
 let anbefalingBtn, narmesteBtn
 let button1, button2, button3
+var apidata
 
 function setup(){
     noCanvas()
@@ -11,7 +12,7 @@ function setup(){
     button3 = select('#button3')
     //html_interaktion
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxiYXRyb3NzZGsiLCJhIjoiY2wyN2NlcWVlMDB1MzNscWhicTh6ZWh2aCJ9.dUMy9S_emcOhW4OHrqONQg';
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxiYXRyb3NzZGsiLCJhIjoiY2wyN2NlcWVlMDB1MzNscWhicTh6ZWh2aCJ9.dUMy9S_emcOhW4OHrqONQg';
 
     const map = new mapboxgl.Map({
         container: 'map', // container ID
@@ -19,49 +20,64 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYWxiYXRyb3NzZGsiLCJhIjoiY2wyN2NlcWVlMDB1MzNsc
         center: [12.568759518980915, 55.66447035003021], // starting position [lng, lat]
         zoom: 10 // starting zoom
     })
+
+    //hent info om badesteder og gem i apidata
+    fetch('http://api.vandudsigten.dk/beaches')
+    .then(res => res.json())
+    .then(json => {
+        apidata = json
+    })
+    
     
     fetch('./index.json')
-    //get response object parse to json()
-    .then( res => res.json() )
-    //when parse done, json object as variable.
-    .then( json => {
-        //console.log(json)
-        data = json.badesteder
+        //get response object parse to json()
+        .then( res => res.json() )
+        //when parse done, json object as variable.
+        .then( json => {
+            console.log(json)
+            data = json.badesteder
 
-    json.badesteder.map( badesteder => {
-        newMarker(badesteder)
-        newCard(badesteder)
-        newPopUp(badesteder)
-    })
+            json.badesteder.map( badesteder => {
+                newMarker(badesteder)
+                newCard(badesteder)
+                newPopUp(badesteder)
+    })    
+    
 })
 
 
 const newMarker = (badested) => {
-    
+    //console.log('running badested: ' + badested)
     let coords = badested.coordinates
     let coordsarray = coords.split(',')
-    console.log(coordsarray)
+    //console.log(coordsarray)
     const marker = new mapboxgl.Marker()
     marker.setLngLat([coordsarray[1],coordsarray[0]])
     marker.addTo(map)
 
     marker.getElement().addEventListener('click', () => {
         alert(badested.name);
-      })
-
+    })
+    
 }
 
 const newCard = (badesteder) => {
-
+   
     //main card structure
     let card = createDiv('')
     let heading = createElement('h2')
     let indhold = createDiv('')
-
+    
     card.addClass('card')
     heading.addClass('heading')
     indhold.addClass('indhold')
-
+    
+    let indholdinfo = createDiv('')
+    let indholdapi = createDiv('')
+    indholdinfo.addClass('indholdinfo')
+    indholdapi.addClass('indholdapi')
+    indhold.child(indholdinfo)
+    indhold.child(indholdapi)
 
     //renlighed
     let heading2 = createElement('h4', 'Badestedets snavsethed:')
@@ -74,9 +90,9 @@ const newCard = (badesteder) => {
 
     progress.style('width', badesteder.renlighed+'%')
     
-    indhold.child(heading2)
+    indholdinfo.child(heading2)
     progressholder.child(progress)
-    indhold.child(progressholder)
+    indholdinfo.child(progressholder)
 
 
     //mennesker
@@ -90,12 +106,13 @@ const newCard = (badesteder) => {
 
     progress1.style('width', badesteder.mennesker+'%')
     
-    indhold.child(heading3)
+    indholdinfo.child(heading3)
     progressholder1.child(progress1)
-    indhold.child(progressholder1)
+    indholdinfo.child(progressholder1)
 
 
     //indhold
+
     heading.html(badesteder.name)
     card.child(heading)
     card.child(indhold)
@@ -136,7 +153,7 @@ const newCard = (badesteder) => {
     let toilet = createDiv('')
     toilet.addClass('toilet')
     usersection.child(toilet)
-    console.log(badesteder.toiletter)
+    //console.log(badesteder.toiletter)
 
     if (badesteder.toiletter == "ja") {
         toilet.style('opacity', '1')
@@ -158,10 +175,60 @@ const newCard = (badesteder) => {
         målgruppe.style('background-image', 'url(./assets/multi.png)')
     }
 
+
+
+    
+    //Vejr card
+
+    //Finds and logs json object of our cards id specified in json
+    let bData = apidata.find( obj => obj.id == badesteder['api-id'])
+    if(bData){
+        console.log('found data: ', bData.data[0])
+    }else{
+        console.log('found no data')
+    }
+
+    //Creates card
+
+    //Created heading for card
+    let headvejr = createElement('h4', 'Vejr')
+    indholdapi.child(headvejr)
+    headvejr.addClass('headvejr')
+    
+    //Created weather
+    let lufttemp = createElement('h5', 'Luft: ' + bData.data[0].air_temperature +'°')
+    indholdapi.child(lufttemp)
+    lufttemp.addClass('lufttemp')
+
+    let vandtemp = createElement('h5', 'Vand: ' + bData.data[0].water_temperature +'°')
+    indholdapi.child(vandtemp)
+    vandtemp.addClass('vandtemp')
+
+    let vindspeed = createElement('h5', 'Vind: ' + bData.data[0].wind_speed +' m/s')
+    indholdapi.child(vindspeed)
+    vindspeed.addClass('vindspeed')
+
+
+    let imgholder = createDiv('')
+    indholdapi.child(imgholder)
+    imgholder.addClass('imgholder')
+    
+    let weathertype = createDiv('')
+    imgholder.child(weathertype)
+    weathertype.addClass('weathertype')
+    weathertype.style('background-image', "url('./assets/weather"+ bData.data[0].weather_type + ".png')")
+
+    let watertype = createDiv('')
+    imgholder.child(watertype)
+    watertype.addClass('watertype')
+    watertype.style('background-image', "url('./assets/water"+ bData.data[0].water_quality + ".png')")
+
+
+
 }
 
 const newPopUp = (badesteder) => {
-    console.log('cum')
+    //console.log('cum')
 }
 
 
@@ -197,4 +264,13 @@ function draw(){
 function createCard(){
 */
 
+}
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+//output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
 }
