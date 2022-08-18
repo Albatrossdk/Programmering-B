f8af5b69d9f30d91a600285fb456c70b53867d93
Albatrossdk <leiferbjorn@gmail.com>
2022-08-18 08:57:19 +0200

    3.d commit api eksempler

--- 2.G/14-rest-api/index.js
+++ 2.G/14-rest-api/index.js
@@ -2,9 +2,10 @@ const getActivity = () => {
     fetch('https://www.boredapi.com/api/activity?type='+ document.querySelector('#pet-select').value)
         .then( response => response.json() )    
         .then( json => createCard(json) )
-}
-
-const createCard = a => {
+    }
+    
+    const createCard = a => {
+    console.log(a)
     document.querySelector('#title').innerHTML = a.activity
     document.querySelector('#participants').innerHTML = 'Participants: ' + a.participants
     document.querySelector('#acc').innerHTML = 'Accesibility: ' + a.accessibility
