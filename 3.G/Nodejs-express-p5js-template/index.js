469868905b80d8005f219d4ad2e5e3be723d3886
Albatrossdk <leiferbjorn@gmail.com>
2022-09-07 23:39:45 +0200

    Websockets!!

--- 3.G/Nodejs-express-p5js-template/index.js
+++ 3.G/Nodejs-express-p5js-template/index.js
@@ -1,22 +1,49 @@
 console.log('Node script now running')
+let colors = ['red', 'green', 'blue', 'purple']
 //hent biblioteket ip
 const ip = require('ip')
 console.log(ip.address())
+//Hent biblioteket socket.io for at lave en websocket
+const socketLib = require('socket.io')
 
 //hent biblioteket express og gem objektet i en konstant
 const express = require('express')
 //initaliser app objektet
 const app = express()
+//json array til brugerinfo
+let users = []
 //definer en port
 const port = 666
 //start en webserver på port 666
-app.listen(port, ()=>{
+const server = app.listen(port, ()=>{
     console.log('Server lytter på port: ' + port)
 })
-
+// opret en server websocket
+const serverSocket = socketLib(server)
 app.use('/', express.static('public'))
 
 //opret et endpoint som returnerer serverens ip
 app.get('/ip', (req, res)=>{
-    res.json('Din ip er: ' + ip.address())
+    res.json(
+        {
+            'ip' : ip.address(),
+            'port' : port
+        }
+    )
+})
+
+serverSocket.sockets.on('connection', socket => {
+    console.log('new socket connection established')
+    //socket.on er en eventlistener på nye beskeder fra klienter
+    socket.on('chat', message => {
+        console.log(message)
+        //når serveren modtager beskeder send den dem rundt til alle. (rundt til alle sockets)
+        serverSocket.sockets.emit('newMessage', message)
+    })
+    socket.on('newUser', user =>{
+        users.push({'name':user, 'id':socket.id})
+        console.log(users)
+
+    })
+
 })
--- 3.G/Nodejs-express-p5js-template/public/index.js
+++ 3.G/Nodejs-express-p5js-template/public/index.js
@@ -1,10 +1,48 @@
+let clientSocket
+let state = 'enterName'
+
 function setup(){
-    //fetch serve rip from node endpoint
+    createCanvas(windowWidth, windowHeight)
+    background('green')
+    
     fetch('http://localhost:666/ip')
-    .then(res => res.json())
-    .then(ip => select('#info').html(ip))
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            select('#info').html(data.ip)
+        })
+        //io kommer fra socket.io biblioteket som vi linker til i html filen
+        clientSocket = io.connect()
+        clientSocket.on('newMessage', message => {
+            let p = createElement('p', message)
+            select('#chat').child(p)
+            select('#chat').elt.scrollTop = select('#chat').elt.scrollHeight
+        })
+        select('#nameButton').mousePressed(()=>{
+            console.log('new user - send to server')
+            clientSocket.emit('newUser', select('#name').value())
+            select('#nameBox').addClass('hide')
+            select('#chatBox').removeClass('hide')
+        })
+        
 }
 
 function draw(){
-    //mouseX mouseY frameCount map
+    
+}
+
+function keyPressed(){
+    
+    if(key == 'Enter'){
+        let message = select('#message').value()
+        if(message!=''){
+            //emit tager et emne og noet data
+            clientSocket.emit('chat', message)
+            select('#message').value('')
+        }
+    }
+}
+
+function mousePressed(){
+
 }
