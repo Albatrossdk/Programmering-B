b819f335984efba33be4478ed008d25dd20a1a4f
Albatrossdk <leiferbjorn@gmail.com>
2023-01-30 21:15:02 +0100

    svelte med api og med components inde i components

---
+++ 3.G/05-svelte-components/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Recipe from './components/Recipe.svelte'
+	let recipes = ['pasta al a carte', 'pasta med sild', 'whiskysovs', 'pebersovs', 'saltet sæd']
+</script>
+
+<header>
+	<h1>Web components</h1>
+</header>
+<main>
+	{#each recipes as title, index}
+	<Recipe {title} {index}/>
+	{/each}
+</main>
+
+<style>
+	h1{
+		color: blueviolet;
+		text-transform: uppercase;
+		font-weight: 100;	
+	}
+	main{
+		width: 100vw;
+		height: 100vh;
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+	}
+</style>
---
+++ 3.G/05-svelte-components/src/components/Recipe.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Number from "./Number.svelte";
+    export let title = 'unnamed'
+    export let index = 0
+    let active = true
+	
+</script>
+
+<main >
+	<h1>{title}</h1>
+    <button on:click={()=>active=!active}>Number</button>
+    <div class="number">
+        <Number number={index} active={active}/>
+    </div>
+</main>
+
+<style>
+    main{
+        display: grid;
+        place-items: center;
+        height: 400px;
+        width: 400px;
+        border: 3px solid blueviolet;
+        border-radius: 1rem;
+        margin: 1rem;
+        position: relative;
+    }
+    .number{
+        position: absolute;
+        display: grid;
+        place-items: center;
+        right: 1rem;
+        bottom: 1rem;
+    }
+
+</style>
